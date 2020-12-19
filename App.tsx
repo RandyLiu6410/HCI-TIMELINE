3933679cfcb95de1beaf92affeeee8fa0b10f437
RandyLiu6410 <wert6410@gmail.com>
2020-12-20 02:01:44 +0800

    Login View

--- App.tsx
+++ App.tsx
@@ -1,9 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { SafeAreaView, StyleSheet, Text, View, Button, Dimensions, processColor } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, TextInput, View, TouchableOpacity, Dimensions, processColor } from 'react-native';
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import BottomSheet from 'reanimated-bottom-sheet';
+
 import { FontAwesome5 } from '@expo/vector-icons';
+import { AppLoading } from 'expo';
+import { useFonts } from 'expo-font';
 import Constants from 'expo-constants';
 
 import HomepageLayout from './src/layouts/homepage/homepage.layout';
@@ -30,30 +34,21 @@ const Theme = {
   },
 };
 
-export default function App() {
+export interface HomeProps {
+  
+}
+
+const Home: React.FC<HomeProps> = (props) => {
   const [mainRoute, setMainRoute] = React.useState(true);
   const [routeChild, setRouteChild] = React.useState('');
   const Tab = createBottomTabNavigator();
 
   return (
     <SafeAreaView style={styles.container}>
-      {/* <Header
-        hasChild={!mainRoute}
-        child={routeChild}
-      /> */}
       <NavigationContainer theme={Theme}>
         <Tab.Navigator
         screenOptions={({ route }) => ({
           tabBarIcon: ({ focused, color, size }) => {
-            // let iconName;
-
-            // if (route.name === 'home' && focused) {
-            //   console.log('home')
-            // } else {
-            //   console.log('other')
-            // }
-
-            // You can return any component that you like here!
             return <FontAwesome5 name={route.name} size={size} color={color} />;
           }
         })}
@@ -94,13 +89,230 @@ export default function App() {
   );
 }
 
+export interface LoginProps {
+  login: any;
+}
+
+const Login: React.FC<LoginProps> = (props) => {
+  const [username, setUsername] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [username_su, setUsername_su] = React.useState('');
+  const [password_su, setPassword_su] = React.useState('');
+  const [confirmpassword_su, setConfirmpassword_su] = React.useState('');
+  const [message, setMessage] = React.useState('');
+  const [message_su, setMessage_su] = React.useState('');
+  const sheetRef = React.useRef(null);
+
+  const renderContent = () => (
+    <View style={styles.bottomSheet}>
+      <Text style={styles.appNameLarge_bs}>TIMELINE</Text>
+      <TextInput 
+        style={styles.textInput_bs}
+        onChangeText={text => setUsername_su(text)}
+        value={username_su}
+        placeholder={'Username'}
+        textContentType='username'
+      />
+      <TextInput 
+        style={styles.textInput_bs}
+        onChangeText={text => setPassword_su(text)}
+        value={password_su}
+        placeholder={'Password'}
+        secureTextEntry={true}
+      />
+      <TextInput 
+        style={styles.textInput_bs}
+        onChangeText={text => {
+          setConfirmpassword_su(text);
+          
+          if(text !== password_su)
+          {
+            setMessage_su('Password and conform password are not same.')
+          }
+          else
+          {
+            setMessage_su('')
+          }
+        }}
+        value={confirmpassword_su}
+        placeholder={'Confirm Password'}
+        secureTextEntry={true}
+      />
+      <TouchableOpacity onPress={signup} disabled={username_su === '' || password_su === '' || confirmpassword_su === '' || password_su !== confirmpassword_su}>
+      <View style={username_su === '' || password_su === '' || confirmpassword_su === '' || password_su !== confirmpassword_su ? styles.loginButton_disable : styles.loginButton}>
+          <Text style={styles.loginText}>Sign Up</Text>
+        </View>
+      </TouchableOpacity>
+      <Text style={styles.message}>{message_su}</Text>
+    </View>
+  );
+
+  let [fontsLoaded] = useFonts({
+    'Audrey': require('./assets/fonts/Audrey/Audrey-Normal.otf')
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+      return (
+          <View style={styles.container}>
+            <View style={styles.loginview}>
+              <Text style={styles.appNameLarge}>TIMELINE</Text>
+              <TextInput 
+                style={styles.textInput}
+                onChangeText={text => setUsername(text)}
+                value={username}
+                placeholder={'Username'}
+                placeholderTextColor={'white'}
+                textContentType='username'
+              />
+              <TextInput 
+                style={styles.textInput}
+                onChangeText={text => setPassword(text)}
+                value={password}
+                placeholder={'Password'}
+                placeholderTextColor={'white'}
+                secureTextEntry={true}
+              />
+              <TouchableOpacity onPress={login} disabled={username === '' && password === ''}>
+                <View style={username === '' || password === '' ? styles.loginButton_disable : styles.loginButton}>
+                  <Text style={styles.loginText}>Login</Text>
+                </View>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => sheetRef.current.snapTo(0)}>
+                <Text style={styles.loginText}>Sign up</Text>
+              </TouchableOpacity>
+              <Text style={styles.message}>{message}</Text>
+            </View>
+            <BottomSheet
+              ref={sheetRef}
+              snapPoints={[350, 0]}
+              borderRadius={10}
+              renderContent={renderContent}
+              initialSnap={1}
+            />
+          </View>
+      );
+  }
+
+  async function login() {
+    await fetch(`http://localhost:8080/user/login?username=${username}&password=${password}`, {
+      method: 'POST'
+    })
+    .then((res) => {
+      props.login(res.ok);
+      res.json().then((m) => setMessage(m));
+    })
+    .catch((err) => {
+    })
+  }
+
+  async function signup() {
+    await fetch(`http://localhost:8080/user?username=${username_su}&password=${password_su}`, {
+      method: 'POST'
+    })
+    .then((res) => {
+      res.json().then((m) => setMessage_su(m));
+
+      if(res.ok)
+      {
+        setTimeout(() => sheetRef.current.snapTo(1), 1000);
+      }
+    })
+    .catch((err) => {
+    })
+  }
+}
+
+export default function App() {
+  const [authorized, setAuthorized] = React.useState(false);
+
+  if(authorized)
+  {
+    return(<Home />)
+  }
+  else{
+    return(<Login login={(auth: boolean) => {setAuthorized(auth)}}/>)
+  }
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#101010',
-    // height: Dimensions.get('window').height
-    // top: Constants.statusBarHeight,
-    // alignItems: 'center',
-    // justifyContent: 'center',
+    backgroundColor: '#101010'
+  },
+  loginview: {
+    marginHorizontal: "20%",
+    marginVertical: "50%"
+  },
+  appNameLarge: {
+    fontFamily: "Audrey",
+    fontSize: 30,
+    color: "#FFFFFF",
+    alignSelf: 'center'
+  },
+  textInput: {
+    height: 40,
+    width: '100%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    color: 'white',
+    padding: 10,
+    marginTop: 10,
+    borderRadius: 10
+  },
+  loginButton: {
+    backgroundColor: '#7B40DC',
+    alignItems: 'center',
+    alignSelf: 'center',
+    alignContent: 'center',
+    width: '30%',
+    marginVertical: 10,
+    height: 40,
+    borderRadius: 10,
+  },
+  loginButton_disable: {
+    backgroundColor: 'gray',
+    alignItems: 'center',
+    alignSelf: 'center',
+    alignContent: 'center',
+    width: '30%',
+    marginVertical: 10,
+    height: 40,
+    borderRadius: 10,
+  },
+  loginText: {
+    color: 'white',
+    fontSize: 15,
+    marginVertical: 10,
+    alignContent: 'center',
+    alignSelf: 'center'
+  },
+  bottomSheet: {
+    backgroundColor: "#FFFFFF",
+    padding: 30
+  },
+  appNameLarge_bs: {
+    fontFamily: "Audrey",
+    fontSize: 30,
+    color: "black",
+    alignSelf: 'center'
+  },
+  textInput_bs: {
+    height: 40,
+    width: '100%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    color: 'black',
+    padding: 10,
+    marginTop: 10,
+    borderRadius: 10
+  },
+  message: {
+    color: 'red',
+    fontSize: 15,
+    marginVertical: 10,
+    alignContent: 'center',
+    alignSelf: 'center'
   }
 });
